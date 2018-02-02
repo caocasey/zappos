633a469883598689e2f17c467c95d97adadab811
Yingxi Cao <caocasey@gmail.com>
2018-02-01 20:03:02 -0500

    add get features

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $items = Item::all();
+        return response()->json([
+            'status'      => 'success',
+            'status_code' => 200,
+            'data'        => $this->transforms($items->toArray()),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Item $item)
+    {
+        //show a specific restaurant info
+        return response()->json([
+        'status'      => 'success',
+        'status_code' => 200,
+        'data'        => $this->transform($item->toArray()),]);
+    }
+
+    //return all tuples
+    private function transforms(array $items)
+    {
+        return array_map([$this, 'transform'], $items);
+    }
+
+    //return singal tuple
+    private function transform(array $item)
+    {
+        return [
+        'itemName'   => $item['iname'],
+        'itemDescription' => $item['idescription'],];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Item $item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Item $item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Item $item)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $restaurants = Restaurant::all();
+        return response()->json([
+            'status'      => 'success',
+            'status_code' => 200,
+            'data'        => $this->transforms($restaurants->toArray()),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Restaurant $restaurant)
+    {
+        //show a specific restaurant info
+        return response()->json([
+        'status'      => 'success',
+        'status_code' => 200,
+        'data'        => $this->transform($restaurant->toArray()),]);
+    }
+
+    //return all tuples
+    private function transforms(array $restaurants)
+    {
+        return array_map([$this, 'transform'], $restaurants);
+    }
+
+    //return singal tuple
+    private function transform(array $restaurant)
+    {
+        return [
+        'restaurantName'   => $restaurant['rname'],
+        'restaurantDescription' => $restaurant['rdescription'],];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Restaurant $restaurant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Restaurant $restaurant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Restaurant $restaurant)
+    {
+        //
+    }
+}
